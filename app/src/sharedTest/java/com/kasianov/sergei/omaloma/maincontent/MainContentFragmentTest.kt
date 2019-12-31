40e225eda3c02a7f6dd114f7bf1e70e54b0c332b
KasSerg <sergei.kasianov@parkman.io>
2019-12-31 11:49:53 +0200

    Some changes

--- app/src/sharedTest/java/com/kasianov/sergei/omaloma/maincontent/MainContentFragmentTest.kt
+++ app/src/sharedTest/java/com/kasianov/sergei/omaloma/maincontent/MainContentFragmentTest.kt
@@ -43,7 +43,7 @@ class MainContentFragmentTest {
         onView(withId(R.id.fab_open_profile)).perform(click())
 
         // THEN - Verify that we navigate to the ProfileFragment screen
-        Mockito.verify(navController).navigate(MainContentFragmentDirections.actionContentMainToProfile().actionId)
+        Mockito.verify(navController).navigate(MainContentFragmentDirections.actionContentMainToHolidaysList().actionId)
     }
 
 }
