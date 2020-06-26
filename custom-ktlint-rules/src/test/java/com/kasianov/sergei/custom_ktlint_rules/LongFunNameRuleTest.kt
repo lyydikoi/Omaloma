20f09c6ba2726123ba7e3637675038149d9185a6
Sergei Kasianov <lyydikoi@gmail.com>
2020-06-27 02:28:09 +0300

    Add custom Ktlint rule for too long fun name.

---
+++ custom-ktlint-rules/src/test/java/com/kasianov/sergei/custom_ktlint_rules/LongFunNameRuleTest.kt
@@ -0,0 +1,27 @@
+package com.kasianov.sergei.custom_ktlint_rules
+
+import com.pinterest.ktlint.core.LintError
+import com.pinterest.ktlint.test.lint
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class LongFunNameRuleTest {
+    @Test
+    fun noWildcardImportsRule() {
+        // whenever KTLINT_DEBUG env variable is set to "ast" or -DktlintDebug=ast is used
+        // com.pinterest.ktlint.test.(lint|format) will print AST (along with other debug info) to the stderr.
+        // this can be extremely helpful while writing and testing rules.
+        System.setProperty("ktlintDebug", "ast")
+
+        Assertions.assertThat(
+            LongFunNameRule().lint("""fun tooLongFunctionNameOverThirtyChars() {}""".trimIndent())
+        ).containsExactly(
+            LintError(
+                1,
+                1,
+                "long-fun-name",
+                "Too long function name tooLongFunctionNameOverThirtyChars!"
+            )
+        )
+    }
+}
